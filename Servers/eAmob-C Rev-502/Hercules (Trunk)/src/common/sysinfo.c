765f468fdc5c295f5ade252eaeb72150d48ef728
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2018-09-12 08:39:54 +0900

    [UPDATE] Header

--- Servers/eAmob-C Rev-502/Hercules (Trunk)/src/common/sysinfo.c
+++ Servers/eAmob-C Rev-502/Hercules (Trunk)/src/common/sysinfo.c
@@ -1,6 +1,26 @@
-// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
-// See the LICENSE file
-// Base Author: Haru @ http://hercules.ws
+/****************************************************************************!
+*                            _                                               *
+*                           / \                         _                    *
+*                   ___    / _ \   _ __ ___   ____  ___| |                   *
+*                  / _ \  / /_\ \ | '_ ` _ \./  _ \/  _  |                   *
+*                 |  __/ /  ___  \| | | | | |  (_) ) (_) |                   *
+*                  \___|/__/   \__\_| |_| |_|\____/\_____/                   *
+*                                                                            *
+*                            eAmod Source File                               *
+*                                                                            *
+******************************************************************************
+* src/common/sysinfo.c                                                       *
+******************************************************************************
+* Copyright (c) eAmod Dev Team                                               *
+* Copyright (c) rAthena Dev Team                                             *
+* Copyright (c) brAthena Dev Team                                            *
+* Copyright (c) Hercules Dev Team                                            *
+* Copyright (c) 3CeAM Dev Team                                               *
+* Copyright (c) Athena Dev Teams                                             *
+*                                                                            *
+* Licensed under GNU GPL                                                     *
+* For more information read the LICENSE file in the root of the emulator     *
+*****************************************************************************/
 
 /// See sysinfo.h for a description of this file
 
