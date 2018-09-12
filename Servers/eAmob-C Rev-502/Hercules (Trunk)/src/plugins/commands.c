a619022db44ecf4f8dec22a28d759942f3f22543
J3MK1 <36283582+J3MK1@users.noreply.github.com>
2018-09-12 09:13:01 +0900

    [UPDATE] Header

--- Servers/eAmob-C Rev-502/Hercules (Trunk)/src/plugins/commands.c
+++ Servers/eAmob-C Rev-502/Hercules (Trunk)/src/plugins/commands.c
@@ -1,8 +1,27 @@
-// Will add all the standard copyright crap later
-//
-// eAmod Commands - Order
-//
-//
+/****************************************************************************!
+*                            _                                               *
+*                           / \                         _                    *
+*                   ___    / _ \   _ __ ___   ____  ___| |                   *
+*                  / _ \  / /_\ \ | '_ ` _ \./  _ \/  _  |                   *
+*                 |  __/ /  ___  \| | | | | |  (_) ) (_) |                   *
+*                  \___|/__/   \__\_| |_| |_|\____/\_____/                   *
+*                                                                            *
+*                            eAmod Source File                               *
+*                                                                            *
+******************************************************************************
+* src/plugins/commands.c                                                     *
+******************************************************************************
+* Copyright (c) eAmod Dev Team                                               *
+* Copyright (c) rAthena Dev Team                                             *
+* Copyright (c) brAthena Dev Team                                            *
+* Copyright (c) Hercules Dev Team                                            *
+* Copyright (c) 3CeAM Dev Team                                               *
+* Copyright (c) Athena Dev Teams                                             *
+*                                                                            *
+* Licensed under GNU GPL                                                     *
+* For more information read the LICENSE file in the root of the emulator     *
+*****************************************************************************/
+
 
 
 #include "../common/cbasetypes.h"
